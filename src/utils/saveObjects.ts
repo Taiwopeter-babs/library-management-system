f53aeb60a809fb5c852c457fb9308adc1fa656b7
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-09-22 12:03:23 +0100

    refactor save function for entities and error handling

--- src/utils/saveObjects.ts
+++ src/utils/saveObjects.ts
@@ -1,12 +1,16 @@
 /// <reference path='./lms.d.ts' />
-import { NewLibrarian, NewBook } from "./interface";
+import { NewLibrarian, NewBook, UserInterface } from "./interface";
 
 import Book from "../controllers/BookController";
 import Librarian from "../controllers/LibControllers";
+import User from "../controllers/UserController";
+
 import dataSource from "./dataSource";
 import { hashPassword } from "./hashVerifyPassword";
 
-export async function createNewLibrarian(librarianObj: NewLibrarian): Promise<boolean> {
+export async function createNewLibrarian(librarianObj: NewLibrarian) {
+  let newLibrarian: Librarian;
+
   try {
     const { name, email, org_email, password } = librarianObj;
     const librarian = new Librarian();
@@ -14,20 +18,42 @@ export async function createNewLibrarian(librarianObj: NewLibrarian): Promise<bo
     librarian.email = email;
     librarian.org_email = org_email;
     librarian.password = await hashPassword(password);
-    await dataSource.saveLibrarian(librarian);
-    return true;
+    newLibrarian = await dataSource.saveEntity(librarian);
   } catch (error) {
-    return false;
+    return null;
   }
+  return newLibrarian;
 }
 
 export async function createNewBook(bookObj: NewBook) {
+  let savedBook: Book;
 
   const { name, quantity, publisher } = bookObj;
   const book = new Book();
   book.name = name;
   book.quantity = quantity;
   book.publisher = publisher;
-  const savedBook = await dataSource.saveBook(book);
+
+  try {
+    savedBook = await dataSource.saveEntity(book);
+  } catch (error) {
+    return null;
+  }
   return savedBook;
+}
+
+export async function createNewUser(userObj: UserInterface) {
+  let savedUser: User;
+
+  const { name, email } = userObj;
+  const user = new User();
+  user.name = name;
+  user.email = email;
+
+  try {
+    savedUser = await dataSource.saveEntity(user);
+  } catch (error) {
+    return null
+  }
+  return savedUser;
 }
