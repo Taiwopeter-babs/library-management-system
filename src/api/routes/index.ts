9e987aafb80224998985c13f15d144214738e1a8
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-09-22 11:54:31 +0100

    update api routes: issue books

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -12,14 +12,19 @@ const router = Router();
 
 // endpoints
 router.get('/users', verifyAccessToken, User.getAllUsers);
+router.get('/users/:userEmail', verifyAccessToken, User.getUser);
+router.post('/users', verifyAccessToken, User.addUser);
+router.post('/users/:userId/books/:bookId', verifyAccessToken, User.addBookToUser);
 // add a book
 router.post('/books', verifyAccessToken, Book.addBook);
+router.get('/books', verifyAccessToken, Book.getAllBooks);
 // update a book
 // delete a book
 // get a book by id
-// get all books - pagination by 25 books by page
+// get all books - pagination by 25 books per page
+
 // librarians authentication
-router.post('/librarians/new', Librarian.postNewLibrarian);
+router.post('/librarians/', Librarian.postNewLibrarian);
 router.post('/librarians/login', Librarian.loginLibrarian);
 router.delete('/librarians/logout', verifyAccessToken, Librarian.logoutLibrarian);
 
