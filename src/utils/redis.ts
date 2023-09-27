354486223bcafde48aee546affd4dacbf24a0b35
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-09-27 15:27:21 +0100

    [add] methods for caching user data in lists and hashes

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,5 +1,7 @@
 import { createClient } from "redis";
 import type { RedisClientType } from "redis";
+import { BookInterface, UserInterface } from "./interface";
+
 
 /**
  * ## redis client for caching
@@ -36,6 +38,64 @@ class RedisClient {
       NX: true,
     });
   }
+
+  async hSet(hashKey: string, fields: any) {
+    try {
+      await this.redisClient.hSet(hashKey, fields);
+    } catch (error) {
+      console.error(error);
+      throw new Error('hSetError');
+    }
+  }
+
+
+  async hGetAll(hashKey: string) {
+    try {
+      const cachedData = await this.redisClient.hGetAll(hashKey);
+      // An object is returned; empty or otherwise
+      if (Object.keys(cachedData).length === 0) {
+        return null;
+      }
+      const { updatedAt, createdAt, name, ...rest } = cachedData;
+      const returnData = {
+        updatedAt: new Date(updatedAt),
+        createdAt: new Date(createdAt),
+        name,
+        ...rest
+      }
+      return returnData;
+    } catch (error) {
+      throw new Error('hGetError');
+    }
+  }
+
+  async listPush(listKey: string, array: Array<string>) {
+    try {
+      for await (let item of array) {
+        await this.redisClient.lPush(listKey, item);
+      }
+    } catch (error) {
+      throw new Error('lPushError');
+    }
+  }
+
+  async listRange(key: string | null | undefined) {
+    if (!key) {
+      return null;
+    }
+    try {
+      const result = await this.redisClient.lRange(key, 0, -1);
+      if (!result) {
+        return null;
+      }
+      return result
+    } catch (error) {
+      console.error(error);
+      throw new Error('lRangeError');
+    }
+
+  }
+
 
   /**
   * ### deletes a key in redis
