937b172d569ce826b1fb155124c8e72c13ad4790
Taiwo Babalola Peter <babalolataiwop@gmail.com>
2023-09-27 15:29:03 +0100

    update API routes

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 // middlewares
 import { verifyAccessToken } from '../../middlewares/authAccess';
+import CacheData from '../../middlewares/getSetCacheData';
 // controllers
 import User from '../../controllers/UserController';
 import Librarian from '../../controllers/LibControllers';
@@ -15,8 +16,8 @@ const router = Router();
  */
 // USERS
 router.get('/users', verifyAccessToken, User.getAllUsers);
-router.get('/users/:userEmail', verifyAccessToken, User.getUserByEmail);
-router.get('/users/id/:userId', verifyAccessToken, User.getUserById);
+router.get('/users/email/:userEmail', verifyAccessToken, User.getUserByEmail);
+router.get('/users/id/:userId', verifyAccessToken, CacheData.getDataFromCache, User.getUserById);
 router.post('/users', verifyAccessToken, User.addUser);
 router.post('/users/:userId/books/:bookId', verifyAccessToken, User.addBookToUser);
 
@@ -25,7 +26,7 @@ router.post('/users/:userId/books/:bookId', verifyAccessToken, User.addBookToUse
  */
 router.post('/books', verifyAccessToken, Book.addBook);
 router.get('/books', verifyAccessToken, Book.getAllBooks);
-router.get('/books/:bookId', verifyAccessToken, Book.getBook);
+router.get('/books/:bookId', verifyAccessToken, CacheData.getDataFromCache, Book.getBook);
 router.put('/books/:bookId', verifyAccessToken, Book.updateBook);
 router.delete('/books/:bookId', verifyAccessToken, Book.deleteBook);
 
